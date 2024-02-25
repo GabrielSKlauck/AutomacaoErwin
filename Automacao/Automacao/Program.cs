6b5e1b0fdaa2e17b52f8456a36deea9b4469c26f
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-02-25 13:01:58 -0300

    Funciona

---
+++ Automacao/Automacao/Program.cs
@@ -0,0 +1,41 @@
+﻿using Automacao;
+using java.util;
+
+WhatsappMsg zap = new WhatsappMsg();
+
+string destino = "Destinatario";
+string texto = @"Nada vai dar certo,- 
+não importa os sonhos e esperanças que tinham..- 
+nao importa o quanto a vida de vocês é abençoada...-
+será tudo igual quando forem esmagados por rochas. -
+Todos morrerão um dia.-
+Quer dizer que a vida não tem sentido?-
+Teve algum sentido em termos nascido?- 
+O que vocês diriam dos nossos companheiros que morreram?- 
+As vidas deles... não tinham sentido?- 
+Não, tinham!- 
+Somos nós que damos significado ás vidas dos nossos companheiros!- 
+Aqueles corajosos caídos!- 
+AFLITOS caídos!- 
+Aqueles que se lembrarão deles somos nós... os vivos!- 
+Morreremos aqui, e deixamos para os que ficarem encontrar significado nas nossas vidas!- 
+Esse é o único método que temos para nós rebelar contra esse mundo Cruel!- 
+Soldados, fúria!- 
+Soldados, gritem!- 
+SOLDADOS LUTEM!";
+
+string fala;
+int indexTraco;
+for (int i = 0; i < texto.Length; i++)
+{
+    indexTraco = texto.IndexOf('-');
+    fala = texto.Substring(0, indexTraco++);
+
+    Thread.Sleep(2000);
+    zap.SendMessage(fala, destino);
+
+    texto = texto.Substring(indexTraco);
+}
+
+
+
---
+++ Automacao/Automacao/WhatsappMsg.cs
@@ -0,0 +1,58 @@
+﻿using EasyAutomationFramework;
+using Newtonsoft.Json.Bson;
+using OpenQA.Selenium;
+using org.bouncycastle.asn1.cms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automacao
+{
+    public class WhatsappMsg : Web
+    {
+        private static bool carregado = false;
+        public WhatsappMsg()
+        {
+            carregarWhats();
+        }
+
+        public void SendMessage(string message, string to)
+        {
+            carregarPessoa(to);
+            if (carregado)
+            {
+                var elementMessage = AssignValue(TypeElement.Xpath, "//*[@id=\"main\"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div[2]/div[1]", message);
+
+                elementMessage.element.SendKeys(Keys.Enter);
+            }
+
+            
+        }
+
+        private void carregarPessoa(string to)
+        {
+            if (!carregado)
+            {
+                var elementSearch = AssignValue(TypeElement.Xpath, "//*[@id=\"side\"]/div[1]/div/div[2]/div[2]/div/div[1]/p", to);
+
+                elementSearch.element.SendKeys(Keys.Enter);
+
+                carregado = true;
+            }
+            
+        }
+
+        private void carregarWhats()
+        {
+            StartBrowser(TypeDriver.GoogleChorme, "C:\\Users\\SeuUsuario\\AppData\\Local\\Google\\Chrome\\User Data");
+
+            Navigate("https://web.whatsapp.com/");
+
+            WaitForLoad();
+
+            Thread.Sleep(TimeSpan.FromSeconds(8));
+        }
+    }
+}
